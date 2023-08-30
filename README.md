7c801722ca6931bc3099482c699a189edb5df3a2
NAME <mail@gmail.com>
2023-08-30 13:46:00 -0500

    Spring Cloud Configuration Server chapter 5

---
+++ README.md
@@ -0,0 +1,5 @@
+#Book Spring Microservices In Action
+
+Implementing a Spring Cloud Configuration Server.
+
+Separating the service configuration from the service code.
---
+++ src/test/java/com/msic/configserver/SpringConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msic.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SpringConfigurationServerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
